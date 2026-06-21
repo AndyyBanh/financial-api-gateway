617269f389e1a5e1310bd560d3e7fc4059025a18
AndyyBanh <andybanhab@gmail.com>
2026-06-20 20:00:04 -0400

    feat: account-service and transaction-service unit tests

---
+++ accountservice/src/test/java/com/financialapigateway/accountservice/service/AccountServiceTest.java
@@ -0,0 +1,124 @@
+package com.financialapigateway.accountservice.service;
+
+import com.financialapigateway.accountservice.entity.Account;
+import com.financialapigateway.accountservice.enumeration.Status;
+import com.financialapigateway.accountservice.event.TransactionEvent;
+import com.financialapigateway.accountservice.kafka.TransactionResultProducer;
+import com.financialapigateway.accountservice.repository.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Mock
+    private TransactionResultProducer transactionResultProducer;
+
+    @InjectMocks
+    private AccountService accountService;
+
+    @Test
+    public void shouldFlagAccountsDoNotExist() {
+        TransactionEvent event = new TransactionEvent();
+        event.setTransactionId(UUID.randomUUID());
+        event.setSenderId("123");
+        event.setRecipientId("321");
+        event.setAmount(50f);
+
+        when(this.accountRepository.findByAccountNumber(event.getSenderId())).thenReturn(Optional.empty());
+        when(this.accountRepository.findByAccountNumber(event.getRecipientId())).thenReturn(Optional.empty());
+
+        this.accountService.processTransaction(event);
+
+        verify(this.transactionResultProducer).send(argThat(result ->
+                result.getTransactionId().equals(event.getTransactionId())
+                && result.getStatus() == Status.FAILED
+        ));
+
+        verify(this.accountRepository, never()).save(any());
+    }
+
+    @Test
+    public void shouldFlagInsufficientBalance() {
+        TransactionEvent event = new TransactionEvent();
+        event.setTransactionId(UUID.randomUUID());
+        event.setSenderId("123");
+        event.setRecipientId("321");
+        event.setAmount(50f);
+
+        Optional<Account> senderAcc = Optional.of(new Account());
+        Optional<Account> receiverAcc = Optional.of(new Account());
+
+        when(this.accountRepository.findByAccountNumber(event.getSenderId())).thenReturn(senderAcc);
+        when(accountRepository.findByAccountNumber(event.getRecipientId())).thenReturn(receiverAcc);
+
+        Account sender = senderAcc.get();
+        sender.setBalance(0f);
+
+        this.accountService.processTransaction(event);
+
+        verify(this.transactionResultProducer).send(argThat(result ->
+                result.getTransactionId().equals(event.getTransactionId())
+                && result.getStatus() == Status.FAILED
+        ));
+
+        verify(this.accountRepository, never()).save(any());
+    }
+
+    @Test
+    public void shouldUpdateBalanceOnSuccess() {
+        TransactionEvent event = new TransactionEvent();
+        event.setTransactionId(UUID.randomUUID());
+        event.setSenderId("123");
+        event.setRecipientId("321");
+        event.setAmount(50f);
+
+        Optional<Account> senderAcc = Optional.of(new Account());
+        Optional<Account> receiverAcc = Optional.of(new Account());
+
+        when(this.accountRepository.findByAccountNumber(event.getSenderId())).thenReturn(senderAcc);
+        when(accountRepository.findByAccountNumber(event.getRecipientId())).thenReturn(receiverAcc);
+
+        Account sender = senderAcc.get();
+        sender.setAccountNumber(event.getSenderId());
+        sender.setBalance(100f);
+
+        Account receiver = receiverAcc.get();
+        receiver.setAccountNumber(event.getRecipientId());
+        receiver.setBalance(0f);
+
+        this.accountService.processTransaction(event);
+
+        assertEquals(50f, sender.getBalance());
+        assertEquals(50f, receiver.getBalance());
+
+
+        verify(this.accountRepository).save(argThat(account ->
+            account.getAccountNumber().equals(event.getSenderId())
+            && account.getBalance() == 50f
+        ));
+
+        verify(this.accountRepository).save(argThat(account ->
+           account.getAccountNumber().equals(event.getRecipientId())
+           && account.getBalance() == 50f
+        ));
+
+        verify(this.transactionResultProducer).send(argThat(result ->
+            result.getTransactionId().equals(event.getTransactionId())
+            && result.getStatus() == Status.COMPLETE
+        ));
+    }
+}
---
+++ transactionservice/src/test/java/com/financialapigateway/transactionservice/service/TransactionServiceTest.java
@@ -0,0 +1,145 @@
+package com.financialapigateway.transactionservice.service;
+
+import com.financialapigateway.transactionservice.dto.TransactionDto;
+import com.financialapigateway.transactionservice.entity.Transaction;
+import com.financialapigateway.transactionservice.enumeration.Status;
+import com.financialapigateway.transactionservice.event.TransactionResultEvent;
+import com.financialapigateway.transactionservice.exceptions.AccountNotFoundException;
+import com.financialapigateway.transactionservice.exceptions.InsufficientBalanceException;
+import com.financialapigateway.transactionservice.feignclient.AccountClient;
+import com.financialapigateway.transactionservice.kafka.TransactionProducer;
+import com.financialapigateway.transactionservice.repository.TransactionRepository;
+import com.financialapigateway.transactionservice.response.AccountResponse;
+import feign.FeignException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TransactionServiceTest {
+
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Mock
+    private TransactionProducer transactionProducer;
+
+    @Mock
+    private AccountClient accountClient;
+
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @Test
+    public void shouldThrowAccountNotFoundExceptionWhenAccountNotFound() {
+        TransactionDto input = new TransactionDto();
+        input.setSenderId("123");
+        input.setRecipientId("321");
+
+        when(this.accountClient.getAccountByAccountNumber(input.getSenderId()))
+                .thenThrow(FeignException.NotFound.class);
+
+        assertThrows(AccountNotFoundException.class, () -> this.transactionService.createTransaction(input));
+
+        verify(this.transactionRepository, never()).save(any());
+    }
+
+    @Test
+    public void shouldThrowInsufficientBalanceException() {
+        TransactionDto input = new TransactionDto();
+        input.setSenderId("123");
+        input.setRecipientId("321");
+        input.setAmount(50f);
+
+        AccountResponse sender = new AccountResponse();
+        sender.setAccountNumber(input.getSenderId());
+        sender.setBalance(0f);
+        AccountResponse recipient = new AccountResponse();
+        recipient.setAccountNumber(input.getRecipientId());
+
+        when(this.accountClient.getAccountByAccountNumber(input.getSenderId())).thenReturn(sender);
+        when(this.accountClient.getAccountByAccountNumber(input.getRecipientId())).thenReturn(recipient);
+
+        assertThrows(InsufficientBalanceException.class,
+                () -> this.transactionService.createTransaction(input));
+
+        verify(this.transactionRepository, never()).save(any());
+    }
+
+    @Test
+    public void shouldCreateTransaction() {
+        TransactionDto input = new TransactionDto();
+        input.setSenderId("123");
+        input.setRecipientId("321");
+        input.setAmount(50f);
+
+        AccountResponse sender = new AccountResponse();
+        sender.setAccountNumber(input.getSenderId());
+        sender.setBalance(100f);
+        AccountResponse recipient = new AccountResponse();
+        recipient.setAccountNumber(input.getRecipientId());
+        recipient.setBalance(0f);
+
+        when(this.accountClient.getAccountByAccountNumber(input.getSenderId())).thenReturn(sender);
+        when(this.accountClient.getAccountByAccountNumber(input.getRecipientId())).thenReturn(recipient);
+
+        this.transactionService.createTransaction(input);
+
+        verify(this.transactionRepository).save(argThat(transaction ->
+                transaction.getSenderId().equals(input.getSenderId())
+                && transaction.getRecipientId().equals(input.getRecipientId())
+                && transaction.getAmount() == 50f
+                && transaction.getStatus() == Status.PENDING
+        ));
+
+        verify(this.transactionProducer).send(argThat(event ->
+                    event.getSenderId().equals(input.getSenderId())
+                && event.getRecipientId().equals(input.getRecipientId())
+                && event.getAmount() == 50f
+                && event.getStatus() == Status.PENDING
+        ));
+    }
+
+
+    @Test
+    public void shouldNotUpdateTransaction() {
+        TransactionResultEvent event = new TransactionResultEvent();
+        event.setTransactionId(UUID.randomUUID());
+        event.setStatus(Status.COMPLETE);
+
+        when(this.transactionRepository.findById(event.getTransactionId())).thenReturn(Optional.empty());
+
+        this.transactionService.updateTransactionStatus(event);
+
+        verify(this.transactionRepository, never()).save(any());
+    }
+
+    @Test
+    public void shouldUpdateTransaction() {
+        TransactionResultEvent event = new TransactionResultEvent();
+        event.setTransactionId(UUID.randomUUID());
+        event.setStatus(Status.COMPLETE);
+
+        Optional<Transaction> transactionOptional = Optional.of(new Transaction());
+        when(this.transactionRepository.findById(event.getTransactionId())).thenReturn(transactionOptional);
+
+        Transaction transaction = transactionOptional.get();
+        transaction.setTransactionId(event.getTransactionId());
+        transaction.setStatus(Status.PENDING);
+
+        this.transactionService.updateTransactionStatus(event);
+
+        verify(this.transactionRepository).save(argThat(res ->
+            res.getTransactionId().equals(event.getTransactionId())
+            &&    res.getStatus() == Status.COMPLETE
+        ));
+    }
+}
